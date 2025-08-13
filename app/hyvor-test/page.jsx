1f6b23f393fb0d35640a1f31ca2df8b3f80cc2d9
chahalltek <chahall@gmail.com>
2025-08-13 19:50:54 -0400

    s

---
+++ app/hyvor-test/page.jsx
@@ -0,0 +1,12 @@
+// app/hyvor-test/page.jsx
+"use client";
+import HyvorComments from "@/components/HyvorComments";
+
+export default function Page() {
+  return (
+    <div className="container py-10">
+      <h1 className="text-2xl font-bold mb-4">Hyvor Test</h1>
+      <HyvorComments pageId="test:hello" title="Hyvor Test" />
+    </div>
+  );
+}
