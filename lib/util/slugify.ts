354d2d35362a626c7e749931804403b66b6044b7
chahalltek <chahall@gmail.com>
2025-08-30 14:45:36 -0400

    s

---
+++ lib/util/slugify.ts
@@ -0,0 +1,9 @@
+// lib/util/slugify.ts
+export function slugify(s: string) {
+  return s
+    .toLowerCase()
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
